d4f8724e6d8ae8b84dabf57ff7ff99acd309bc69
Jimmy <arts.jimmy@gmail.com>
2020-01-26 10:23:01 +0100

    Fix helm. Added status route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
+    app.get("status") { req -> HTTPStatus in
+        return .ok
+    }
+    
     let authController = AuthController(userClient: app.userClient, hydraClient: app.hydraClient)
     try app.register(collection: authController)
 }
